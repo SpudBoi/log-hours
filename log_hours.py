d3462ed5c1469012fdd0e97dc75772b26dc46a9d
Conor Muldoon <conormmuldoon@gmail.com>
2019-09-05 13:21:49 -0700

    add browser cancellation and remove testing passes

--- log_hours.py
+++ log_hours.py
@@ -29,6 +29,70 @@ def input_info():
 
     #closing the window
     window.destroy()
+    execute_logging()
+
+
+def execute_logging():
+    #create web driver and navigate to call manager
+    driver = webdriver.Chrome()
+    driver.get("https://my.pugetsound.edu/psp/PA91PRD/EMPLOYEE/EMPL/h/?tab=UP_SIGNIN_TAB")
+    driver.implicitly_wait(10)
+
+    #enter username
+    web_username = driver.find_element_by_name("userid")
+    web_username.click()
+    web_username.send_keys(username)
+
+    #enter password and login
+    web_password = driver.find_element_by_name("pwd")
+    web_password.click()
+    web_password.send_keys(password)
+    web_password.send_keys(Keys.RETURN)
+
+    time.sleep(5)
+    driver.find_element_by_xpath("//*[@id='win1divPTGP_STEPS_L1_row$2']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//*[@id='win1divPTGP_STEPS_L2_row$12']").click()
+
+    week_days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+    # //*[@id="win0divDAY_OF_WK_DISPLAY$0"] //*[@id="win0divPUNCH_DATE_DISPLAY$0"] //*[@id="PUNCH_TIME_1$0"] //*[@id="PUNCH_TIME_2$0"] //*[@id="TRC$0"]
+    # //*[@id="win0divDAY_OF_WK_DISPLAY$1"] //*[@id="win0divPUNCH_DATE_DISPLAY$1"] //*[@id="PUNCH_TIME_1$1"] //*[@id="PUNCH_TIME_2$1"] //*[@id="TRC$1"]
+    frame = driver.find_element_by_xpath("//*[@id='main_target_win0']")
+    driver.switch_to.frame(frame)
+    index = -1
+    for i in range(0,17):
+        xpath = "//*[@id='PUNCH_DATE_DISPLAY$" + str(i) + "']"
+        if driver.find_element_by_xpath(xpath).text == date:
+            index = i
+            break
+
+    xpath1 = "//*[@id='PUNCH_TIME_1$" + str(index) + "']"
+    xpath2 = "//*[@id='PUNCH_TIME_2$" + str(index) + "']"
+    xpath3 = "//*[@id='PUNCH_TIME_3$" + str(index) + "']"
+    xpath4 = "//*[@id='PUNCH_TIME_4$" + str(index) + "']"
+
+    driver.find_element_by_xpath(xpath1).click()
+    driver.find_element_by_xpath(xpath1).clear()
+    driver.find_element_by_xpath(xpath1).send_keys("11:00AM")
+
+    # driver.find_element_by_xpath(xpath2).click()
+    # driver.find_element_by_xpath(xpath2).clear()
+    # driver.find_element_by_xpath(xpath2).send_keys("12:00PM")
+    #
+    # driver.find_element_by_xpath(xpath3).click()
+    # driver.find_element_by_xpath(xpath3).clear()
+    # driver.find_element_by_xpath(xpath3).send_keys("12:30PM")
+
+    driver.find_element_by_xpath(xpath4).click()
+    driver.find_element_by_xpath(xpath4).clear()
+    driver.find_element_by_xpath(xpath4).send_keys("2:00PM")
+
+    xpath5 = "//select[@id='TRC$" + str(index) + "']/option[text()='STE - Regular Pay - Student FICA Ex']"
+    driver.find_element_by_xpath(xpath5).click()
+
+    driver.find_element_by_xpath("//*[@id='TL_LINK_WRK_SUBMIT_PB$418$']").click()
+    time.sleep(10)
 
 ################################################################################
 #creating initial window
@@ -62,64 +126,3 @@ tkinter.Button(window, image = login, bg = "white", command = input_info, border
 #initiate gui
 window.mainloop()
 ################################################################################
-
-
-#create web driver and navigate to call manager
-driver = webdriver.Chrome()
-driver.get("https://my.pugetsound.edu/psp/PA91PRD/EMPLOYEE/EMPL/h/?tab=UP_SIGNIN_TAB")
-driver.implicitly_wait(10)
-
-#enter username
-web_username = driver.find_element_by_name("userid")
-web_username.click()
-web_username.send_keys(username)
-
-#enter password and login
-web_password = driver.find_element_by_name("pwd")
-web_password.click()
-web_password.send_keys(password)
-web_password.send_keys(Keys.RETURN)
-
-time.sleep(5)
-driver.find_element_by_xpath("//*[@id='win1divPTGP_STEPS_L1_row$2']").click()
-time.sleep(2)
-driver.find_element_by_xpath("//*[@id='win1divPTGP_STEPS_L2_row$12']").click()
-
-week_days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
-
-# //*[@id="win0divDAY_OF_WK_DISPLAY$0"] //*[@id="win0divPUNCH_DATE_DISPLAY$0"] //*[@id="PUNCH_TIME_1$0"] //*[@id="PUNCH_TIME_2$0"] //*[@id="TRC$0"]
-# //*[@id="win0divDAY_OF_WK_DISPLAY$1"] //*[@id="win0divPUNCH_DATE_DISPLAY$1"] //*[@id="PUNCH_TIME_1$1"] //*[@id="PUNCH_TIME_2$1"] //*[@id="TRC$1"]
-frame = driver.find_element_by_xpath("//*[@id='main_target_win0']")
-driver.switch_to.frame(frame)
-index = -1
-for i in range(0,17):
-    xpath = "//*[@id='PUNCH_DATE_DISPLAY$" + str(i) + "']"
-    if driver.find_element_by_xpath(xpath).text == date:
-        index = i
-        break
-
-xpath1 = "//*[@id='PUNCH_TIME_1$" + str(index) + "']"
-xpath2 = "//*[@id='PUNCH_TIME_2$" + str(index) + "']"
-xpath3 = "//*[@id='PUNCH_TIME_3$" + str(index) + "']"
-xpath4 = "//*[@id='PUNCH_TIME_4$" + str(index) + "']"
-
-driver.find_element_by_xpath(xpath1).click()
-driver.find_element_by_xpath(xpath1).clear()
-driver.find_element_by_xpath(xpath1).send_keys("11:00AM")
-
-driver.find_element_by_xpath(xpath2).click()
-driver.find_element_by_xpath(xpath2).clear()
-driver.find_element_by_xpath(xpath2).send_keys("12:00PM")
-
-driver.find_element_by_xpath(xpath3).click()
-driver.find_element_by_xpath(xpath3).clear()
-driver.find_element_by_xpath(xpath3).send_keys("12:30PM")
-
-driver.find_element_by_xpath(xpath4).click()
-driver.find_element_by_xpath(xpath4).clear()
-driver.find_element_by_xpath(xpath4).send_keys("2:00PM")
-
-xpath5 = "//select[@id='TRC$" + str(index) + "']/option[text()='STE - Regular Pay - Student FICA Ex']"
-driver.find_element_by_xpath(xpath5).click()
-
-#driver.find_element_by_xpath("//*[@id='TL_LINK_WRK_SUBMIT_PB$418$']").click()
